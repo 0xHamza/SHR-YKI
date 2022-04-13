7822e007a3803c6aee87c3e9449c3ba8f5f3859d
0xHamza <62814321+0xHamza@users.noreply.github.com>
2022-04-13 15:44:47 +0300

    Add files via upload

---
+++ SHR/siniflar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
+
+public class Sample
+{
+    public string vocabulary { get; set; }
+    public string meaning { get; set; }
+    public int number { get; set; }
+    public int group { get; set; }
+}
